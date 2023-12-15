71b3b7aa0e6efc298791fc683792d3d398fa0d14
Neeharika Baral <neehabaral72@gmail.com>
2023-12-15 00:05:55 -0500

    refractor getUserWithId function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -36,6 +36,17 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
+  return pool
+  .query(`
+    SELECT * FROM users
+    WHERE id = $1
+  `, [id])
+  .then(res => {
+    return res.rows.length === 0 ? null : res.rows[0];
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
 };
 
 /**
